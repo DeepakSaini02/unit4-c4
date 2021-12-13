53bcae9380cf817e1736107545705e1210159df1
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2021-12-13 16:56:03 +0530

    Update show.controller.js

--- src/controllers/show.controller.js
+++ src/controllers/show.controller.js
@@ -3,6 +3,8 @@ const User=require('../models/user.model')
 const Movie=require('../models/movie.model')
 const upload=require('../middlewares/upload')
 const Show=require('../models/show.model')
+const Screen=require('../models/screen.model')
+const Theatre=require('../models/theatre.model')
 
 // const User=require('../models/user.model')
 const express=require('express')
@@ -25,7 +27,16 @@ router.get("/:movie",async(req,res)=>{
         return res.status(500).json({status:"failed",message:e.message})
     }
 })
-
+router.get("/shows/:location",async(req,res)=>{
+    try{
+        const theatre=await Theatre.find({location:req.params.location}).lean().exec()
+        const screen=await Screen.find({theatre:theatre}).lean().exec()
+        const show=await Show.find({screen:screen}).populate("screen").lean().exec()
+        return res.status(201).send({show})
+    }catch(e){
+        return res.status(500).json({status:"failed",message:e.message})
+    }
+})
 
 
 
